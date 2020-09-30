74088b016bb9110a528880a7936637233eb58235
Warren Uhrich <hello@warren.codes>
2020-09-30 10:31:35 -0600

    Add single and double quote string examples.

--- hello-world.php
+++ hello-world.php
@@ -21,3 +21,14 @@ echo "Hello, World!";
 // (Also, don't start them with numbers or special characters.)
 $studentName = "\nFahad";
 echo $studentName;
+
+$studentName2 = '\nDanish';
+echo $studentName2;
+
+// Single quotes will NOT evalute special character escapes or
+// variable values within the string.
+echo '\n\n Two students in TECHCareers are:\n- $studentName\n- $studentName2.';
+
+// Double quotes, WILL though! Careful which one you use for
+// your current use-case!
+echo "\n\nTwo students in TECHCareers are:\n$studentName\n$studentName2";
