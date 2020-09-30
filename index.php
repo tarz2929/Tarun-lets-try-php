9536f35ee628cbcdbe12773acd8ed11707f8723f
Warren Uhrich <hello@warren.codes>
2020-09-30 13:24:19 -0600

    Add assignment and echo to index.php.

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
-<!DOCTYPE html>
+<?php
+  // Assigning a variable.
+  $pageTitle = 'TECHCareers | Basic PHP Webpage';
+?><!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Basic PHP Webpage</title>
+  <title><?php echo $pageTitle; // We can echo it, or change it, wherever we need to! ?></title>
 
   <!-- Style(s) -->
   <link rel="stylesheet" type="text/css" href="css/main.css">
@@ -12,6 +15,6 @@
   <script type="text/JavaScript" src="js/scripts.js" defer></script>
 </head>
 <body>
-  <h1>Basic PHP Webpage</h1>
+  <h1><?php echo $pageTitle; ?></h1>
 </body>
 </html>
