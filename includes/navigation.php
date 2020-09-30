ca155aa6c260952c33521d8db2d4f4f725606a59
Warren Uhrich <hello@warren.codes>
2020-09-30 13:51:43 -0600

    Add include examplel add navigation.

---
+++ includes/navigation.php
@@ -0,0 +1,17 @@
+<?php
+  $navArray = array(
+    'Home' => 'index.php',
+    'About' => 'about.php'
+  );
+?><nav>
+  <h2>Website Navigation</h2>
+  <ul>
+    <?php foreach ( $navArray as $pageName => $link ) : ?>
+      <li>
+        <a href="<?php echo $link; ?>">
+          <?php echo $pageName; ?>
+        </a>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+</nav>
