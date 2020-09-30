05ddfb978a54ab9c54dc3659716706b4e22de182
Warren Uhrich <hello@warren.codes>
2020-09-30 10:12:20 -0600

    Add example comments.

--- hello-world.php
+++ hello-world.php
@@ -1,1 +1,14 @@
-<?php echo "Hello, World!";
+<?php
+// "Echo" and "print" are functions we can...
+// use to output strings as a response.
+echo "Hello, World!";
+
+// There are two other styles of comment we can use in PHP. This (//) is single-line.
+
+# This is also a single-line comment.
+
+/* This is our
+      (familiar)
+          multi-line
+  comment! */
+  
